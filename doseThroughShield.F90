4f12829a3de513e3d2bb7841f6b716a276bdb4ec
cmdPromptCritical <carlisle.of.richards@gmail.com>
2018-02-11 16:30:49 -0500

    Working, outputs flux rate. Next step: dose rate

--- doseThroughShield.F90
+++ doseThroughShield.F90
@@ -8,17 +8,17 @@ x (shield thickness, m),
 r (distance from source to target, m)
 #endif
 ! -----------------------------------------------Declare
-INTEGER sourceActivity, photonE, x, r, k
+INTEGER k, j
 REAL, dimension(:), allocatable :: murhodb(:,:)
 INTEGER :: n
-REAL flux, linInterp, murho, materialDensity
+REAL flux, linInterp, murho, shieldDensity, photonE, x, r, sourceActivity
 
 real tempC, tempF, FACTOR
 integer ZERO_SHIFT
 parameter (ZERO_SHIFT = 32, FACTOR = 5./9.)
 ! -----------------------------------------------Input
 ! Defines shield density (g/cm^3)
-materialDensity = 7.874
+shieldDensity = 7.874
 
 print*, "Enter the source activity, in Bq"
 read*, sourceActivity
@@ -26,7 +26,7 @@ read*, sourceActivity
 print*, "Enter the photon energy, in MeV"
 read*, photonE
 
-print*, "Enter the shield thickness, in meters"
+print*, "Enter the shield thickness, in centimeters"
 read*, x
 
 print*, "Enter the distance from the source to the target, in meters"
@@ -46,11 +46,13 @@ end do
 
 
 ! -----------------------------------------------Compute
-print*, linInterp(2.0,3.0,4.0,2.0,6.0)
-print*, murho(2.5, murhodb, n)
+print*, linInterp(1.0,5.0,10.0,20.0,40.0), "Linear interpolation"
+print*, (5 - 1) * (40 - 20) / (10 - 1) + 10, "Linear interpolation, manual"
+!print*, murhodb()
+print*, murho(photonE, murhodb, n)
 
-flux = (sourceActivity * exp(3.0))
-
+flux = (sourceActivity * exp(-1.0*murho(photonE, murhodb, n)*shieldDensity*x)) / (4.0*3.14159*r**2.0)
+print*, flux
 ! -----------------------------------------------Output
 print*, "The corresponding Centigrade temperature is "
 print*, flux, " degrees."
@@ -60,10 +62,11 @@ FUNCTION linInterp(x1, x2, x3, y1, y3) result(y2)
   REAL x1, x2, x3, y1, y2, y3
 
   y2 = (x2 - x1) * (y3 - y1) / (x3 - x1) + y1
+  return
 END FUNCTION linInterp
 
 FUNCTION murho(energy, murhodb, n) result(ans)
-  INTEGER i, j, n
+  INTEGER i, n
   REAL energy, mu, linInterp, x1, x3, y1, y3
   REAL, dimension(n, 2) :: murhodb
 
@@ -74,10 +77,11 @@ FUNCTION murho(energy, murhodb, n) result(ans)
       y3 = murhodb(i,2)
       x1 = murhodb(i-1,1)
       y1 = murhodb(i-1,2)
+      exit
     end if
 
     i = i + 1
   end do
 
-  ans = linInterp(x1, x2, x3, y1, y3)
+  ans = linInterp(x1, energy, x3, y1, y3)
 END FUNCTION murho
